f279c01202498231ddd2fbf6b694f0c3d39eda37
Ianel Tombozafy <ianell.s118@gmail.com>
2022-04-14 16:26:24 +0300

    feat: create input for phone number

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <input type="tel" placeholder="Entrer votre numéro de telephone" />
+      <button>S'isncrire</button>
+    </div>
+  );
+};
+
+export default LoginPage;
