1a82de405338f2196b389b83c98f47061b166df3
Ianel Tombozafy <ianell.s118@gmail.com>
2022-04-19 17:30:02 +0300

    asset: add new image for homepage logo

--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -1,5 +1,6 @@
 import avatar from "@/assets/images/avatar1.png";
 import mac from "@/assets/images/mac.jpg";
 import logo from "@/assets/images/logo3.png";
+import logoBlue from "@/assets/images/logo-mpanera.png";
 
-export { avatar, mac, logo };
+export { avatar, mac, logo, logoBlue };
