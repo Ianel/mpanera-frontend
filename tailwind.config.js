62a4afb744ea72081731f8a12c16012685a7689e
Roel Tombozafy <rtombozafy96@gmail.com>
2022-05-20 07:42:15 +0300

    Add black and white colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,14 @@ module.exports = {
         primary: "#8EA5D9",
         secondary: "#414E73",
         accent: "#536DFE",
+
+        primaryWhite: "#f6f6f6",
+        secondaryWhite: "#fefefe",
+        accentWhite: "#fdfeff",
+
+        primaryDark: "#303030",
+        secondaryDark: "#2b2b2b",
+        accentDark: "1f1f1f",
       },
     },
   },
