a71c637a894ff326fec431fcc024ef3624d02b30
Ianel Tombozafy <ianell.s118@gmail.com>
2022-08-17 17:00:27 +0300

    add update function to profile page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -16,6 +16,7 @@ import usersService from "../../services/users.service";
 import states from "../../states";
 import toast from "react-hot-toast";
 import { useSnapshot } from "valtio";
+import moment from "moment";
 
 const ProfilePage = () => {
   const [show, setShow] = useState(false);
@@ -67,11 +68,11 @@ const ProfilePage = () => {
                 <p className="flex flex-row items-center gap-3">
                   <FaMapMarkerAlt />
                   {singleUser.adress ? singleUser.adress : "Adresse"}{" "}
-                  {singleUser.region ? singleUser.region : "region"}{" "}
-                  {singleUser.city ? singleUser.city : "Ville"}{" "}
+                  {singleUser.city ? singleUser.city : "Ville"},{" "}
                   {singleUser.postal_code
                     ? singleUser.postal_code
-                    : "code postal"}{" "}
+                    : "code postal"}
+                  ,{singleUser.region ? singleUser.region : "region"},{" "}
                   {singleUser.country ? singleUser.country : "pays"}
                 </p>
                 <p className="flex flex-row items-center gap-3">
@@ -97,14 +98,16 @@ const ProfilePage = () => {
             <div className="w-2/3 py-4 mx-auto">
               <h2 className="text-lg font-bold">A propos</h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
-                vitae, provident repudiandae fugiat error vel natus totam
-                excepturi cumque inventore debitis eum. Veritatis quia
-                recusandae consequatur unde libero sapiente placeat! Lorem ipsum
-                dolor sit amet consectetur adipisicing elit. Totam dolore ex a
-                ratione, amet inventore dicta deserunt! Ipsum, deserunt sapiente
-                corrupti non error beatae magnam voluptatibus veniam nostrum
-                illum. Numquam.
+                {singleUser["about"]
+                  ? singleUser["about"]
+                  : `Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+                  vitae, provident repudiandae fugiat error vel natus totam
+                  excepturi cumque inventore debitis eum. Veritatis quia
+                  recusandae consequatur unde libero sapiente placeat! Lorem ipsum
+                  dolor sit amet consectetur adipisicing elit. Totam dolore ex a
+                  ratione, amet inventore dicta deserunt! Ipsum, deserunt sapiente
+                  corrupti non error beatae magnam voluptatibus veniam nostrum
+                  illum. Numquam.`}
               </p>
             </div>
           </div>
@@ -122,7 +125,6 @@ const ProfilePage = () => {
 const ProfileForm = () => {
   const [gender, setGender] = useState("");
   const [userId, setUserId] = useState(0);
-  const [user, setUser] = useState({});
 
   const snapshot = useSnapshot(states);
 
@@ -132,7 +134,25 @@ const ProfileForm = () => {
 
     await usersService
       .getUser(id)
-      .then((user) => setUser(user.data.results))
+      .then((user) => {
+        setGender(user.data.results.gender);
+
+        states.input.profileFirstname = user.data.results.firstname;
+        states.input.profileLastname = user.data.results.lastname;
+        states.input.profileBirthdate = new Date(
+          user.data.results.birthdate
+        ).toLocaleDateString("en-gb");
+        states.input.profileCity = user.data.results.city;
+        states.input.profilePostalCode = user.data.results.postal_code;
+        states.input.profileRegion = user.data.results.region;
+        states.input.profileCountry = user.data.results.country;
+        states.input.profileAdress = user.data.results.adress;
+        states.input.profilePhoneNumber = user.data.results.phone_number;
+        states.input.profileEmail = user.data.results.email;
+        states.input.profileFacebookName = user.data.results.facebook_name;
+        states.input.profileAvatar = user.data.results.profile_avatar;
+        states.input.profileAbout = user.data.results.about;
+      })
       .catch((error) => console.error(error));
   }, []);
 
@@ -141,6 +161,10 @@ const ProfileForm = () => {
       <h2 className="text-lg font-bold text-center mb-4">
         Veuillez remplir les champs suivants
       </h2>
+      {console.log(
+        new Date(snapshot.input.profileBirthdate).toLocaleDateString("da_DK")
+      )}
+
       <form>
         <div className="grid grid-cols-2 gap-3">
           <label htmlFor="lastname" className="flex flex-col">
@@ -150,6 +174,7 @@ const ProfileForm = () => {
               name="profileLastname"
               type="text"
               placeholder="Nom"
+              value={snapshot.input.profileLastname}
             />
           </label>
           <label htmlFor="firstname" className="flex flex-col">
@@ -159,6 +184,7 @@ const ProfileForm = () => {
               name="profileFirstname"
               type="text"
               placeholder="Prénom"
+              value={snapshot.input.profileFirstname}
             />
           </label>
           <label htmlFor="gender" className="flex flex-col">
@@ -170,6 +196,7 @@ const ProfileForm = () => {
               onChange={(e) => {
                 setGender(e.target.value);
               }}
+              value={gender}
             >
               <option>Choisir votre sexe</option>
               <option value="Homme">Homme</option>
@@ -178,11 +205,19 @@ const ProfileForm = () => {
           </label>
           <label htmlFor="birthdate" className="flex flex-col">
             Date de naissance
-            <Input id="birthdate" name="profileBirthdate" type="date" />
+            <Input
+              id="birthdate"
+              value={moment(snapshot.input.profileBirthdate).format(
+                "yyyy-MM-dd"
+              )}
+              name="profileBirthdate"
+              type="date"
+            />
           </label>
           <label htmlFor="city" className="flex flex-col">
             Ville
             <Input
+              value={snapshot.input.profileCity}
               id="city"
               name="profileCity"
               type="text"
@@ -192,6 +227,7 @@ const ProfileForm = () => {
           <label htmlFor="postalCode" className="flex flex-col">
             Code postal
             <Input
+              value={snapshot.input.profilePostalCode}
               id="postalCode"
               name="profilePostalCode"
               type="text"
@@ -201,6 +237,7 @@ const ProfileForm = () => {
           <label htmlFor="region" className="flex flex-col">
             Région
             <Input
+              value={snapshot.input.profileRegion}
               id="region"
               name="profileRegion"
               type="text"
@@ -210,6 +247,7 @@ const ProfileForm = () => {
           <label htmlFor="country" className="flex flex-col">
             Pays
             <Input
+              value={snapshot.input.profileCountry}
               id="country"
               name="profileCountry"
               type="text"
@@ -219,6 +257,7 @@ const ProfileForm = () => {
           <label htmlFor="adress" className="flex flex-col">
             Adresse
             <Input
+              value={snapshot.input.profileAdress}
               id="adress"
               name="profileAdress"
               type="text"
@@ -228,6 +267,7 @@ const ProfileForm = () => {
           <label htmlFor="phoneNumber" className="flex flex-col">
             Numéro de télephone
             <Input
+              value={snapshot.input.profilePhoneNumber}
               id="phoneNumber"
               type="tel"
               name="profilePhoneNumber"
@@ -237,6 +277,7 @@ const ProfileForm = () => {
           <label htmlFor="email" className="flex flex-col">
             Email
             <Input
+              value={snapshot.input.profileEmail}
               id="email"
               name="profileEmail"
               type="email"
@@ -246,44 +287,50 @@ const ProfileForm = () => {
           <label htmlFor="facebook_name" className="flex flex-col">
             Nom facebook
             <Input
+              value={snapshot.input.profileFacebookName}
               id="facebook_name"
               name="profileFacebookName"
               type="text"
               placeholder="Nom Facebook"
             />
           </label>
-          <label htmlFor="profilePicture" className="flex flex-col">
+          <label htmlFor="profileAvatar" className="flex flex-col">
             Photo de profil
             <Input
-              id="profilePicture"
-              name="profilePicture"
+              value={snapshot.input.profileAvatar}
+              id="profileAvatar"
+              name="profileAvatar"
               type="file"
               className="border-none"
             />
           </label>
-          <label htmlFor="city" className="flex flex-col">
+          <label htmlFor="profileAbout" className="flex flex-col">
             A propos
-            <TextArea name="profileAbout" placeholder="A propos"></TextArea>
+            <TextArea
+              id="profileAbout"
+              name="profileAbout"
+              placeholder="A propos"
+            ></TextArea>
           </label>
         </div>
         <button
           onClick={() => {
             const userData = {
               user_id: userId,
-              firstname: states.input.profileFirstname,
-              lastname: states.input.profileLastname,
+              firstname: snapshot.input.profileFirstname,
+              lastname: snapshot.input.profileLastname,
               gender: gender,
-              birthdate: states.input.profileBirthdate,
-              city: states.input.profileCity,
-              postal_code: states.input.profilePostalCode,
-              region: states.input.profileRegion,
-              country: states.input.profileCountry,
-              adress: states.input.profileAdress,
-              phone_number: states.input.profilePhoneNumber,
-              email: states.input.profileEmail,
-              facebook_name: states.input.profileFacebookName,
-              profile_picture: states.input.profilePicture,
-              about: states.input.profileAbout,
+              birthdate: snapshot.input.profileBirthdate,
+              city: snapshot.input.profileCity,
+              postal_code: snapshot.input.profilePostalCode,
+              region: snapshot.input.profileRegion,
+              country: snapshot.input.profileCountry,
+              adress: snapshot.input.profileAdress,
+              phone_number: snapshot.input.profilePhoneNumber,
+              email: snapshot.input.profileEmail,
+              facebook_name: snapshot.input.profileFacebookName,
+              profile_avatar: snapshot.input.profileAvatar,
+              about: snapshot.input.profileAbout,
             };
 
             usersService
